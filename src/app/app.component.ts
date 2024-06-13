64354d45541d36e59d43a2235a086e289cef0d48
Jorge Barriga <alonsoxbarriga@hotmail.com>
2024-06-13 12:44:25 -0500

    add NavBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {RegistrarClienteComponent} from "./component/registrar-cliente/registrar-cliente.component";
 import {RegistrarOstComponent} from "./component/registrar-ost/registrar-ost.component";
+import {NavbarComponent} from "./component/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RegistrarClienteComponent, RegistrarOstComponent],
+  imports: [RouterOutlet, RegistrarClienteComponent, RegistrarOstComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+// @ts-ignore
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import {RegistrarClienteComponent} from "./component/registrar-cliente/registrar-cliente.component";
+import {RegistrarOstComponent} from "./component/registrar-ost/registrar-ost.component";
+
+export const routes: Routes = [
+  {path: 'registrar-cliente', component:RegistrarClienteComponent},
+  {path: 'registrar-ost', component:RegistrarOstComponent},
+
+
+];
