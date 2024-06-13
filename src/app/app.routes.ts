1d7ea704fd70003941d7d49b6521e6246b5d1779
Jorge Barriga <alonsoxbarriga@hotmail.com>
2024-06-13 12:57:11 -0500

    principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 // @ts-ignore
 
 import { Routes } from '@angular/router';
+import { PrincipalComponent } from './component/principal/principal.component';
 import {RegistrarClienteComponent} from "./component/registrar-cliente/registrar-cliente.component";
 import {RegistrarOstComponent} from "./component/registrar-ost/registrar-ost.component";
 
 export const routes: Routes = [
+  {path: '', redirectTo: '/principal', pathMatch: 'full'},
   {path: 'registrar-cliente', component:RegistrarClienteComponent},
   {path: 'registrar-ost', component:RegistrarOstComponent},
-
-
 ];
